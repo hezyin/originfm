9d47d71e7e86b3f9286e73e45d6db80e9b0ae22a
UltimateBeaver <tlhly@hotmail.com>
2015-01-20 19:57:18 +0800

    fix a minor bug

--- ptuner/optuner_0120.rb
+++ ptuner/optuner_0120.rb
@@ -15,7 +15,7 @@ f = open("ptuner_ruby_log", "w+")
 
 parameters.each_with_index do |p, i|
   f.puts "#{i+1}/#{total} k=#{p[0]} e1=#{p[1]} e2=#{p[2]} l1=#{p[3]} l2=#{p[4]} #{(Time.now - start)/60}mins"
-  `../fm -k #{p[0]} -t 10 -s 8 -e1 #{p[1]} -e2 #{p[2]} -l1 #{p[3]} -l2 #{p[4]} -g k_#{p[0]}_e1_#{p[1]}_e2_#{p[2]}_l1_#{p[3]}_l2_#{p[4]} train.libfm test.libfm`
+  `../fm -k #{p[0]} -t 10 -s 8 -e1 #{p[1]} -e2 #{p[2]} -l1 #{p[3]} -l2 #{p[4]} -g k_#{p[0]}_e1_#{p[1]}_e2_#{p[2]}_l1_#{p[3]}_l2_#{p[4]} ../train.libfm ../test.libfm`
 end
 
 f.close
